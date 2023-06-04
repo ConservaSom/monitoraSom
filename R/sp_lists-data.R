bb3398494038a7f358552927d27b2f8513a1388c
GLMRosa <g.rosa.bio@gmail.com>
2023-06-04 15:03:39 -0300

    added sp_lists into data

---
+++ R/sp_lists-data.R
@@ -0,0 +1,18 @@
+#' Lists of species for labelling regions of interest
+#'
+#' A data frame containing
+#'
+#' @docType data
+#'
+#' @usage data(sp_lists)
+#'
+#' @format ## `sp_lists`
+#'
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{`CBRO-2021 (Brazil)`}{List of species from Brazil}
+#'   \item{`Aves Argentinas`}{List of species from Argentina}
+#'   ...
+#' }
+#' @source Todo
+"sp_lists"
